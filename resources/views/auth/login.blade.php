3c05a3d15fd2357a2a19382f7d47feebd4474af3
Jeff, Jeong  정 의정 <35421421+TuenTuenna@users.noreply.github.com>
2020-04-23 01:44:36 +0900

    로그인,회원가입 레이아웃변경

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,39 @@
-@extends('layouts.app')
+@extends('layouts.blog_app')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+@section('blog_content')
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+    <!-- Three -->
+    <section id="three" class="wrapper style3 special">
+        <div class="container">
+            <header class="major">
+                <h2>로그인 하기</h2>
+            </header>
         </div>
-    </div>
-</div>
+        <div class="container 50%">
+
+            <form action="/login" method="post">
+                @csrf
+                <div class="row uniform">
+                    <div class="12u 12u$(small)">
+                        <label for="email" class="col-md-4 col-form-label text-md-left" style="color: white">이메일</label>
+                        <input name="email" id="email" value="" placeholder="이메일을 입력해주세요.." type="email">
+                    </div>
+
+                    <div class="12u 12u$(small)">
+                        <label for="password" class="col-md-4 col-form-label text-md-left" style="color: white">비밀번호</label>
+                        <input name="password" id="password" value="" placeholder="비밀번호를 입력해주세요.." type="password">
+                    </div>
+
+                    <div class="12u$">
+                        <ul class="actions">
+                            <li><input value="로그인" class="special big" type="submit"></li>
+                        </ul>
+                    </div>
+                </div>
+            </form>
+
+        </div>
+    </section>
+
+
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,50 @@
-@extends('layouts.app')
+@extends('layouts.blog_app')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+@section('blog_content')
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+    <!-- Three -->
+    <section id="three" class="wrapper style3 special">
+        <div class="container">
+            <header class="major">
+                <h2>회원가입 하기</h2>
+            </header>
         </div>
-    </div>
-</div>
+        <div class="container 50%">
+
+            <form action="/register" method="post">
+                @csrf
+                <div class="row uniform">
+
+                    <div class="12u 12u$(small)">
+                        <label for="name" class="col-md-4 col-form-label text-md-left" style="color: white">이름</label>
+                        <input name="name" id="name" value="" placeholder="이름을 입력해주세요.." type="text">
+                    </div>
+
+                    <div class="12u 12u$(small)">
+                        <label for="email" class="col-md-4 col-form-label text-md-left" style="color: white">이메일</label>
+                        <input name="email" id="email" value="" placeholder="이메일을 입력해주세요.." type="email">
+                    </div>
+
+                    <div class="12u 12u$(small)">
+                        <label for="password" class="col-md-4 col-form-label text-md-left" style="color: white">비밀번호</label>
+                        <input name="password" id="password" value="" placeholder="비밀번호를 입력해주세요.." type="password">
+                    </div>
+
+                    <div class="12u 12u$(small)">
+                        <label for="password-confirm" class="col-md-4 col-form-label text-md-left" style="color: white">비밀번호 확인</label>
+                        <input name="password_confirmation" required autocomplete="new-password" id="password-confirm" value="" placeholder="비밀번호를 한번 더 입력해주세요.." type="password">
+                    </div>
+
+                    <div class="12u$">
+                        <ul class="actions">
+                            <li><input value="회원가입" class="special big" type="submit"></li>
+                        </ul>
+                    </div>
+                </div>
+            </form>
+
+        </div>
+    </section>
+
+
 @endsection
